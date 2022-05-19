079186994db33135da780401c25b130b749690b2
unknown <kaitlandevans@gmail.com>
2022-05-18 21:12:58 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+class bankAccount {
+  constructor(){
+    this.accountNumber = this.accountNumber;
+    this.owner = this.owner;
+    this.transactions = this.transactions;
+  }
+}
+
+class transaction {
+  constructor(){
+    this.date = date;
+    this.amount = amount;
+    this.payee = payee;
+  }
+}
